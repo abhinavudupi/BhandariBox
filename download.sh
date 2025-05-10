a99d02643713edb2b0076078d62e315fb5699247
Abhinav Udupi <66860832+abhinavudupi@users.noreply.github.com>
2025-05-10 13:24:26 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@ wget https://github.com/abhinavudupi/BhandariBox/archive/refs/heads/main.zip
 unzip main.zip
 mv BhandariBox-main/ ContribWebUI/
 rm main.zip
+chown -R bhandaribox:bhandaribox ContribWebUI
 cd ContribWebUI
 chmod 777 run.sh
 apt-get update
